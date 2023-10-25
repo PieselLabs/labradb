cf9ea118f3765f0dc79accaa4370a67ea18ed089
Artem Alekseev <fexolm@gmail.com>
2023-10-25 11:02:47 +0300

    Try another stdlib functions

--- codegen/lib.rs
+++ codegen/lib.rs
@@ -1,6 +1,12 @@
+use std::time::Instant;
+
 #[no_mangle]
 pub extern "C" fn sum(a: i32, b: i32) -> i32 {
-    println!("Hello world!");
+    let now = Instant::now();
+
+    let elapsed_time = now.elapsed();
+
+    println!("Hello world: {}!", elapsed_time.as_secs());
 
     a + b
 }
